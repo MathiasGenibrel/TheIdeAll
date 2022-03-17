ae64c3635577edda88f824fd98765c0a68ac2cf8
MathiasGenibrel <mathias.geni@gmail.com>
2022-03-17 14:34:49 +0100

    feat(LoginForm) finish login Form

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Input.css";
 import EmailIcon from "../assets/icons/Email.svg.js";
 import PasswdIcon from "../assets/icons/Password.svg.js";
 
 const Input = (props) => {
+  const [value, setValue] = useState("");
+
+  const valueChangeHandler = (event) => {
+    console.log(event.target.value)
+    setValue(event.target.value);
+  };
+
   const icon =
     props.icon === "email"
       ? EmailIcon
@@ -12,12 +19,12 @@ const Input = (props) => {
       ? PasswdIcon
       : null;
 
-      console.log(icon)
   return (
     <label htmlFor={`inputForm${props.type}`} className="Input">
       <p>{props.title}</p>
       <article>
         <input
+          onChange={valueChangeHandler}
           id={`inputForm${props.type}`}
           type={props.type}
           placeholder={props.placeholder}
