153c9a8be2c5aa8ddad75ab5c4fc850200abcf37
GaspardBrisset <brissetgaspard@gmail.com>
2022-03-17 23:42:49 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- backend/index.php
+++ backend/index.php
@@ -1,27 +1,55 @@
 <?php
-  // Initialiser la session
-  session_start();
-  // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
-  if(!isset($_SESSION["pseudo"])){
-    header("Location: login.php");
-    exit(); 
-  }
-?>
+
+
+
+require_once('./Controller/DatabaseLinker.php');
+
+setcookie('user_id', '1234',time()+36000); 
+  ?>
 <!DOCTYPE html>
 <html>
   <head>
-  <link rel="stylesheet" href="assets/styles/style.css" />
+  
   </head>
   <body>
-    <?php
-    require_once("assets/header.php");
-    ?>
+
     <div class="sucess">
-    <h1>Bienvenue <?php echo $_SESSION['pseudo']; ?>!</h1>
-    <p><a href ="pages/formUrl.php">C'est votre tableau de bord.</a></p>
-    <a href="login/logout.php">Déconnexion</a>
-    </div>
+    <h1>Bienvenue </h1>
+    
+    <p>C'est votre tableau de bord.</p>
     <?php
-    require_once("assets/footer.php");
-    ?>
+    $pdo = DataBaseLinker::getDataBaseLinker();
+      $users =$pdo->afficherUtilisateurs();
+      $JSONResponse = [];
+
+      foreach ($users as $index => $user)
+      {
+        $userData = [];
+        foreach ($user as $columnName => $value) 
+        {
+            if (!is_numeric($columnName)) $userData[$columnName] = $value;
+        }
+        array_push($JSONResponse, $userData);
+      }
+      print_r(json_encode($JSONResponse));
+     ?>
+
+    
+    <?php
+    $pdo = DataBaseLinker::getDataBaseLinker();
+    $users =$pdo->cookieIdentification();
+    print_r($users);
+   /* $email=$_POST["email"];
+    $motDePasse=$_POST["motDePasse"];*/
+
+
+    /*if (isset($_COOKIE["user_id"]))
+    echo 'Le cookie existe ' . $_COOKIE["user_id"] . '!<br />';
+    else
+    echo 'Le cookie n\'existe pas <br />';/*$instance = new UtilisateursController();
+      $appelNormal = $instance->afficherUtilisateurs(); ?>
+    <?php  echo ("<script>console.log( 'Debug Objects: " . $testtest. "' );</script>")*/
+  ?>
+    </div>
+    
   
